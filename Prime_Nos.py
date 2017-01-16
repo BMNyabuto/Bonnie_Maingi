4c576e3310341475df7784a1db6f2e3c701f8ca5
boman <bmnyabuto@gmail.com>
2017-01-17 00:53:59 +0300

    Lists prime numbers without smallest prime (2)

--- Prime_Nos.py
+++ Prime_Nos.py
@@ -1,3 +1,11 @@
 def PrimeFunc(n):
-	""" Lists the Prime NUmbers between 2 and the parameter """
+	""" Prints a list containing Prime Numbers between 2 and the defined parameter (n)"""
+	RetnList = [True]* n
+	for x in range(3,int(n**0.5) + 1, 2):
+		if RetnList[i]:
+		
+			RetnList[i*i::2*i] = [False]*((n-1*i-1)/(2*i)+1)
+				
+	return [i for i in range(3, n, 2) if RetnList[i]]
+		
 		
