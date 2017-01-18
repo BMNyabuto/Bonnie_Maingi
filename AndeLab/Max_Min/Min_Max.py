07ae16dd76ab1436d885fe215601d1d26a1a7fd9
boman <bmnyabuto@gmail.com>
2017-01-18 14:02:44 +0300

    Added Min_Max.py

---
+++ AndeLab/Max_Min/Min_Max.py
@@ -0,0 +1,17 @@
+def find_max_min(ArrayValues):
+	Result = []
+	if isinstance(ArrayValues, list) == True:
+		ArrayValues.sort()	
+		minValue = ArrayValues[0]
+		maxValue = ArrayValues[-1]
+            
+        Result.append(minValue)
+
+		if ArrayValues[-1] == minValue:
+		return Result
+
+		else:
+		Result.append(maxValue)
+		return Result
+	else:
+		raise TypeError('Parameter provided is not a list')
